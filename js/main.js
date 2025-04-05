c885b95f340598cf16f9bd6526adc4c67e3ee897
aldeantonio <aldeantonio8@gmail.com>
2025-04-05 17:58:09 +0200

    Change sending features

--- js/main.js
+++ js/main.js
@@ -1,51 +1,31 @@
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+document.getElementById('meu-formulario').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+  const form = e.target;
+  const botao = document.getElementById('botao-enviar');
+  const mensagem = document.getElementById('mensagem-estado');
+
+  botao.disabled = true;
+  mensagem.textContent = 'A enviar...';
+
+  const formData = new FormData(form);
+
+  fetch('https://formspree.io/f/movekgan', {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
     }
-  }
-}
-
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
+  }).then(response => {
+    if (response.ok) {
+      mensagem.textContent = 'Dados enviados com sucesso!';
+      form.reset();
+    } else {
+      mensagem.textContent = 'Ocorreu um erro. Tente novamente.';
     }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    botao.disabled = false;
+  }).catch(error => {
+    mensagem.textContent = 'Erro ao enviar. Verifique a ligação.';
+    botao.disabled = false;
   });
-}
-
-
-
-
+});
